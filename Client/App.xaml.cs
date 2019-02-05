6308c6820be6edd8f6a4764598ec02b29a88b58c
Tomas_Light <pikachu-94@yandex.ru>
2019-02-06 01:03:45 +0300

    launcher: start develop (WIP)

---
+++ Client/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //StartupUri = new Uri("/Client;component/Windows/MainWindow.xaml", UriKind.Relative);
+            StartupUri = new Uri("/Client;component/Windows/MainMenuWindow.xaml", UriKind.Relative);
+        }
+    }
+}
